327bc932d1aa5080583bc5dbe7aa1d871c97582a
WRyanBai <68730565+WRyanBai@users.noreply.github.com>
2020-08-05 19:11:05 +0800

    Update iteration.py

--- iteration.py
+++ iteration.py
@@ -1,7 +1,8 @@
+import math
 import pycuber as pc
 import random as r
 from tqdm import tqdm
-from cube_utilities import action_map, flatten_string
+from cube_utilities import action_map, flatten_string, cube_completeness
 
 
 def generate_sequence(prob = 100):
@@ -10,8 +11,7 @@ def generate_sequence(prob = 100):
     scrambled_cube = pc.Cube()
     actions = []
     states_cube_list = []
-    distance_list = []
-    state_reward=[]
+    value_list = []
     # The following lines scrambles the cubes using 3 actions
     for i in range(3):
         actions.append(r.choice(list(action_map.keys())))
@@ -22,34 +22,35 @@ def generate_sequence(prob = 100):
         # cube is a copy of the scrambled_cube
         # the scrambled state is being kept in scrambled_cube while cube is being solved
         cube = scrambled_cube.copy()
+        total_steps = 0
         random_number = r.randint(0, 100)
         if random_number <= prob:
             # The following code solves the cube by reversing the actions that it was scrambled with
             formula.reverse()
-            for k, s in enumerate(formula):
+            for k, move in enumerate(formula):
                 # k is the current number of actions taken to solve the cube
                 # e.g after performing one action on cube, k is 0. after two actions it is 1
                 states_cube_list.append(cube.copy())
-                distance_list.append(3 - k)
-                ##reward=##write reward formula here
-                ##state_reward.append(reward)
-                cube(s)
-            return states_cube_list, distance_list,state_reward
+                cube(move)
+                total_steps += 1
         else:
-            update_cube=cube.copy() #update_cube is the updates every step and keeps track of the best moves
-            for i in range(3):
-                best_move="U"
-                max_score=0
-                for move in possible_actions:
-                    init_cube=update_cube.copy()
+            while total_steps < 6 and cube_completeness(cube) < 1 :
+                best_move = "U"
+                max_value = 0
+                for move in action_map.keys():
+                    init_cube = cube.copy()
                     init_cube(move)
-                    if(flatten_string(init_cube) in cube_reward):
-                        if(cube_reward([flatten_string(init_cube)])>max_score):
-                            best_move=move
-                states_cube_list.append(update_cube.copy())
-                distance_list.append(3-move)
-                update_cube(best_move)
-            return states_cube_list,distance_list,state_reward
+                    if flatten_string(init_cube) in cube_reward and cube_reward[flatten_string(init_cube)] > max_value:
+                        best_move = move
+                states_cube_list.append(cube.copy())
+                cube(best_move)
+                total_steps += 1
+        final_reward = 100 * (cube_completeness(cube) == 1) - 5 * total_steps
+        for step, state in enumerate(states_cube_list):
+            value = final_reward * math.pow(0.9, step + 1)
+            value_list.append(value)
+    return states_cube_list, value_list
+
 
 
 for i in range(1):
@@ -63,9 +64,9 @@ for i in range(1):
     state_reward = []
     state_reward.append(_state_reward)
     ##
-    ##for c in tqdm(distance_to_solved):
-    ##   rewards = (100 - distance_to_solved[0]) * (1 - (0.1 * int(c)))
-    ##    state_reward.append(rewards)  # 每一state的reward
+    # for c in tqdm(distance_to_solved):
+    #    rewards = (100 - distance_to_solved[0]) * (1 - (0.1 * int(c)))
+    #     state_reward.append(rewards)  # 每一state的reward
 
     for j in range(len(cubes)):
         # if the state is already in cube_reward, its value in cube_reward will be the greater value
