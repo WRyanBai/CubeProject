5a799c443e58b18c4208e6baf5be46976317925f
Asdfghjkl-ops <59274283+Asdfghjkl-ops@users.noreply.github.com>
2020-08-05 01:41:34 -0400

    Add files via upload

--- iteration.py
+++ iteration.py
@@ -11,6 +11,7 @@ def generate_sequence(prob = 100):
     actions = []
     states_cube_list = []
     distance_list = []
+    state_reward=[]
     # The following lines scrambles the cubes using 3 actions
     for i in range(3):
         actions.append(r.choice(list(action_map.keys())))
@@ -30,8 +31,10 @@ def generate_sequence(prob = 100):
                 # e.g after performing one action on cube, k is 0. after two actions it is 1
                 states_cube_list.append(cube.copy())
                 distance_list.append(3 - k)
+                ##reward=##write reward formula here
+                ##state_reward.append(reward)
                 cube(s)
-            return states_cube_list, distance_list
+            return states_cube_list, distance_list,state_reward
         else:
             update_cube=cube.copy() #update_cube is the updates every step and keeps track of the best moves
             for i in range(3):
@@ -46,8 +49,7 @@ def generate_sequence(prob = 100):
                 states_cube_list.append(update_cube.copy())
                 distance_list.append(3-move)
                 update_cube(best_move)
-            return states_cube_list,distance_list
-            # solve the cube using trained values, not completed yet
+            return states_cube_list,distance_list,state_reward
 
 
 for i in range(1):
@@ -55,15 +57,15 @@ for i in range(1):
     distance_to_solved = []
     cube_reward = {}
     for j in tqdm(range(10)):
-        _cubes, _distance_to_solved = generate_sequence()
+        _cubes, _distance_to_solved,_state_reward = generate_sequence()
         cubes.extend(_cubes)
         distance_to_solved.extend(_distance_to_solved)
-
     state_reward = []
-
-    for c in tqdm(distance_to_solved):
-        rewards = (100 - distance_to_solved[0]) * (1 - (0.1 * int(c)))
-        state_reward.append(rewards)  # 每一state的reward
+    state_reward.append(_state_reward)
+    ##
+    ##for c in tqdm(distance_to_solved):
+    ##   rewards = (100 - distance_to_solved[0]) * (1 - (0.1 * int(c)))
+    ##    state_reward.append(rewards)  # 每一state的reward
 
     for j in range(len(cubes)):
         # if the state is already in cube_reward, its value in cube_reward will be the greater value
