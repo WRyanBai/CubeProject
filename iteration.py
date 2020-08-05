5508b067318dd25a74e08e94c6fc194f64c873fb
WRyanBai <68730565+WRyanBai@users.noreply.github.com>
2020-08-06 01:35:14 +0800

    Update iteration.py

--- iteration.py
+++ iteration.py
@@ -40,6 +40,7 @@ def generate_sequence(prob = 100):
                     init_cube(move)
                     if flatten_string(init_cube) in cube_reward and cube_reward[flatten_string(init_cube)] > max_value:
                         best_move.append(move)
+                        max_value = cube_reward[flatten_string(init_cube)]
                 states_cube_list.append(cube.copy())
                 cube(r.choice(best_move))
                 total_steps += 1
@@ -91,6 +92,7 @@ while total_steps < 6 and cube_completeness(test_cube) < 1:
         init_cube(move)
         if flatten_string(init_cube) in cube_reward and cube_reward[flatten_string(init_cube)] > max_value:
             best_move = move
+            max_value = cube_reward[flatten_string(init_cube)]
     test_cube(best_move)
     total_steps += 1
 print(test_cube)
